19bedf7826150d3f997656322e8134e3894382cf
im23b-terenzianie <terenzianie@bzz.ch>
2025-03-06 16:28:38 +0100

    transaction class created

--- transaction.java
+++ transaction.java
@@ -1,3 +1,41 @@
+import java.lang.reflect.Type;
+
 public class transaction {
 
+    private String category;
+    private double amount;
+    private String type;
+
+    // Konstruktor
+    public transaction(String category, double amount, String type){
+        this.category = category;
+        this.amount = amount;
+        this.type = type;
+    }
+    // Getter
+    public String getCategory(){
+        return category;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    //Setter
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
